bcdc984ea53427653dfc1070720abccfe59c109c
Naoki Komazawa <komazawa@ekzm.co.jp>
2023-08-22 11:58:28 +0900

    ページネーションを実装

--- src/lib/menuItems.js
+++ src/lib/menuItems.js
@@ -1,6 +1,6 @@
 export const menuItems = [
   { label: 'about', href: '/about/' },
-  { label: 'blog', href: '/blog/' },
+  { label: 'blog', href: '/blog/page/' },
   { label: 'company', href: '/company/' },
   { label: 'contact', href: '/contact/' },
 ]
